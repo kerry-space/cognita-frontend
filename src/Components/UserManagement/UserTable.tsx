9ef9377bfbb3d8092e6dc74aa877dbbcbde46892
maxth <maxthorstorp@gmail.com>
2024-09-29 13:10:12 +0200

    add some styling

--- src/Components/UserManagement/UserTable.tsx
+++ src/Components/UserManagement/UserTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IUser } from '../../utils';
-
+import './css/UserTable.css';
 interface IUserTableProps {
   editClick: (user: IUser) => void;
   deleteClick: (userId: number) => void;
@@ -9,26 +9,42 @@ interface IUserTableProps {
 
 export function UserTable({ editClick, deleteClick, data }: IUserTableProps) {
   return (
-    <table className='table'>
+    <table className='table user-table'>
       <thead>
         <tr>
-          <th scope='col'>Role</th>
-          <th scope='col'>Name</th>
-          <th scope='col'>Email</th>
-          <th scope='col'>Course</th>
-          <th scope='col'>Actions</th>
+          <th className='table-cell column-header' scope='col'>
+            Role
+          </th>
+          <th className='table-cell column-header' scope='col'>
+            Name
+          </th>
+          <th className='table-cell column-header' scope='col'>
+            Email
+          </th>
+          <th className='table-cell column-header' scope='col'>
+            Course
+          </th>
+          <th className='table-cell column-header' scope='col'>
+            Actions
+          </th>
         </tr>
       </thead>
       <tbody>
         {data.map(user => (
-          <tr key={user.email}>
-            <td>{user.role}</td>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td>{user.course}</td>
-            <td>
-              <button onClick={() => editClick(user)}>Edit</button>
-              <button onClick={() => deleteClick(user.id)}>Delete</button>
+          <tr key={user.id}>
+            <td className='table-cell role-cell'>{user.role}</td>
+            <td className='table-cell'>{user.name}</td>
+            <td className='table-cell'>{user.email}</td>
+            <td className='table-cell'>{user.course}</td>
+            <td className='table-cell btn-container'>
+              <button className='edit-btn' onClick={() => editClick(user)}>
+                Edit
+              </button>
+              <button
+                className='delete-btn'
+                onClick={() => deleteClick(user.id)}>
+                Delete
+              </button>
             </td>
           </tr>
         ))}
