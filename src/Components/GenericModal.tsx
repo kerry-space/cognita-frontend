9aa7d2e4bc8460b54f33f7743f00b609a44a5784
maxth <maxthorstorp@gmail.com>
2024-10-03 11:34:55 +0200

    small update after feedback

--- src/Components/GenericModal.tsx
+++ src/Components/GenericModal.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
-import "./GenericModel.css"
-
 interface GenericModalProps {
   show: boolean;
   title: string;
@@ -11,24 +9,38 @@ interface GenericModalProps {
   children: React.ReactNode;
 }
 
-function GenericModal({ show, title, handleClose, handleSave, children }: GenericModalProps) {
+function GenericModal({
+  show,
+  title,
+  handleClose,
+  handleSave,
+  children,
+}: GenericModalProps) {
   return (
-    <Modal show={show} onHide={handleClose} style={{ display: 'flex', alignItems: 'center', minHeight: 'calc(100vh - 60px)' }}>
-  <Modal.Dialog style={{ width: '100%', maxWidth: '600px', margin: '0 auto' }}>
-    <Modal.Header closeButton>
-      <Modal.Title>{title}</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>{children}</Modal.Body>
-    <Modal.Footer>
-      <Button variant="secondary" onClick={handleClose}>
-        Close
-      </Button>
-      <Button variant="primary" onClick={handleSave}>
-        Save Changes
-      </Button>
-    </Modal.Footer>
-  </Modal.Dialog>
-</Modal>
+    <Modal
+      show={show}
+      onHide={handleClose}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        minHeight: 'calc(100vh - 60px)',
+      }}>
+      <Modal.Dialog
+        style={{ width: '100%', maxWidth: '600px', margin: '0 auto' }}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant='primary' onClick={handleSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </Modal>
   );
 }
 
