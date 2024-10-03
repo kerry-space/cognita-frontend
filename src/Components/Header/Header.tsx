7098fa502a574b825abb7b2f760cd5744054e740
maxth <maxthorstorp@gmail.com>
2024-10-03 14:05:46 +0200

    fix navbar links

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,20 +1,27 @@
-import React, { ReactElement } from "react";
-import "./Header.css";  // Ensure this points to the correct CSS file
-import logo from "../../assets/Cognita-logo.png"
-
+import { ReactElement } from 'react';
+import './Header.css'; // Ensure this points to the correct CSS file
+import logo from '../../assets/Cognita-logo.png';
+import { Link } from 'react-router-dom';
+import { useAuthContext } from '../../Hooks';
 
 export function Header(): ReactElement {
-    console.log("Header rendered");
-    return (
-        <nav className="navbar">
-            <a className="navbar-brand" href="#">
-                <img src={logo} alt="Cognita" className="navbar-logo" />
-            </a>
-            <div className="navbar-links">
-                <a className="nav-link" href="#">Home</a>
-                <a className="nav-link" href="#">UserManagement</a>
-                <a className="nav-link" href="#">LogOut</a>
-            </div>
-        </nav>
-    );
+  const { logout } = useAuthContext();
+  return (
+    <nav className='navbar'>
+      <a className='navbar-brand' href='#'>
+        <img src={logo} alt='Cognita' className='navbar-logo' />
+      </a>
+      <div className='navbar-links'>
+        <Link className='nav-link' to={'/'}>
+          Home
+        </Link>
+        <Link className='nav-link' to={'/user-management'}>
+          User Management
+        </Link>
+        <a onClick={logout} className='nav-link'>
+          Logout
+        </a>
+      </div>
+    </nav>
+  );
 }
